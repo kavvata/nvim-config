5cc6db781e26bd8e9f7dbc5a730ede04382b63be
kavvata <gabriel.kawata@gmail.com>
2024-04-20 22:32:13 -0300

    enable phpactor to blade files

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,6 +2,13 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     setup = {
+      phpactor = function(_, opts)
+        opts.filetypes = {
+          "php",
+          "blade",
+        }
+      end,
+
       emmet_language_server = function(_, opts)
         opts.filetypes = {
           "css",
