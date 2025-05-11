8c38e2233fa400cd93ba8e779b43d8e7487cf183
kavvata <gabriel.kawata@gmail.com>
2025-05-11 19:07:09 -0300

    Lazyvim update, venv-selector, goodbye ruby and pinned mason version

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,11 +2,11 @@ return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
     opts.formatters_by_ft = {
-      lua = { "stylua" },
       fish = { "fish_indent" },
       php = { "pint", "pretty-php" },
       blade = { "blade-formatter" },
       typescriptreact = { "prettier" },
+      typescript = { "prettier" },
     }
   end,
 }
---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,20 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  keys = {
+    { '<leader>vs', '<cmd>VenvSelect<cr>' },
+    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    { ",v",         "<cmd>VenvSelect<cr>" },
+  },
+  ---@type venv-selector.Config
+  opts = {
+    -- Your settings go here
+  },
+}
