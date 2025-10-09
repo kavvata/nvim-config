62d3d74df91b205f5e2a7169688c7e6062acdf6e
kavvata <gabriel.kawata@gmail.com>
2025-10-09 10:28:04 -0300

    fix(treesitter): breaking changes from treesitter. nuked everything

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,13 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      opts.ensure_installed = {
+      vim.list_extend(opts.ensure_installed, {
         "bash",
         "html",
         "javascript",
+        "typescript",
+        "tsx",
+        "jsx",
         "json",
         "lua",
         "markdown",
@@ -23,18 +26,18 @@ return {
         "phpdoc",
         "css",
         "htmldjango",
-      }
-      ---@class parser_config
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
+      })
+      ------@class parser_config
+      ---local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      ---
+      ---parser_config.blade = {
+      ---  install_info = {
+      ---    url = "https://github.com/EmranMR/tree-sitter-blade",
+      ---    files = { "src/parser.c" },
+      ---    branch = "main",
+      ---  },
+      ---  filetype = "blade",
+      ---}
     end,
   },
 }
